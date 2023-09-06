eb86c4f5853ba4299be85c2f6541b9ed03b9a5d4
Fradd747 <korbay.jan@gmail.com>
2023-09-06 08:33:45 +0200

    add template system

--- index.php
+++ index.php
@@ -36,8 +36,15 @@
             <h1 class="font-['skautbold'] text-lg mb-2">Generátor harmonogramu</h1>
             <form action="export.php" method="post" class="flex flex-col gap-3 items-start font-['themix']" enctype="multipart/form-data">
                 <div class="flex flex-col gap-1">
-                    <label for="file">ICS export z Google kalendáře ↓</label>
+                    <label for="file">ICS export z Google kalendáře</label>
                     <input type="file" name="file" id="file" accept=".ics" required>
+                </div>
+                <div class="flex flex-col gap-1">
+                    <label for="template">Šablona</label>
+                    <select name="template" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block min-w-[12rem] w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="default" selected>Výchozí</option>
+                        <option value="ursus">Ursus</option>
+                    </select>
                 </div>
                 <div class="flex flex-col gap-1">
                     <label for="file">Rozsah exportu</label>
---
+++ templates/ursus.php
@@ -0,0 +1,155 @@
+<?php 
+
+    function printEvent(bool $program = false, bool $required = false, DateTime $start, DateTime $end, string $summary, array $description = null, string $classes = '') {    
+        //according to time, calculate height of box. 2rem = 30min
+        $height = (($end->getTimestamp() - $start->getTimestamp()) / 900) * 0.8;
+        $time = $start->format('H:i') .' - '. $end->format('H:i');
+        $food = in_array(trim(strtolower($summary)), $GLOBALS['food']);
+        //var_dump($height);
+        echo '
+            <div class="box '. ($program ? 'gray_box' : '') . ($food ? 'black_box' : '') . ' min-h-['. $height .'rem] ' . $classes .'">
+                <div class="inner_box">        
+                    <p>'. $time .'</p>
+                    <div>
+                        <p class="font-bold">'. $summary .'</p>';
+                        if (!is_null($description)) {
+                            echo '<p style="line-height: 17px">';
+                            foreach ($description as $line) {
+                                echo $line . '<br>';
+                            }
+                        }
+            echo '</div>
+                </div>';
+                if ($required) {
+                    echo '
+                    <div class="flex justify-end items-center pr-3">
+                        <img class="!h-11 !w-11" src="icons/hand.png">
+                    </div>';
+                }
+            echo '</div>';
+    }
+
+?>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com/3.3.0"></script>
+    <style>
+        body {
+            -webkit-print-color-adjust:exact !important;
+            print-color-adjust:exact !important;
+        }
+        @media print {
+            .pagebreak { page-break-before: always; 
+            }
+        }
+        @font-face {
+            font-family: 'themix';
+            src: url('fonts/themix-bold.ttf') format('truetype');
+            font-style: bold;
+            font-weight: normal;
+        }
+        @font-face {
+            font-family: 'themix';
+            src: url('fonts/themix-normal.ttf') format('truetype');
+            font-style: normal;
+            font-weight: normal;
+        }
+        @font-face {
+            font-family: 'skautbold';
+            src: url('fonts/skautbold.ttf') format('truetype');
+            font-style: normal;
+            font-weight: normal;
+        }
+        .box {
+            padding-top: 0.25rem;
+            padding-bottom: 0.25rem; 
+            padding-left: 0.5rem;
+            padding-right: 0.5rem; 
+            padding-left: 0.75rem; 
+            border-radius: 0.5rem;
+            border-width: 1px; 
+            border-color: #1F2937;
+            min-height: 2rem;
+            /* height: fit-content; */
+            display: grid; 
+            font-size: 0.9rem;
+            grid-template-columns: 82% 18%;
+        }
+        .inner_box {
+            display: flex;
+            flex-direction: row;
+            gap: 0.5rem;
+            align-items: start;
+            justify-content: start;
+        }
+        .black_box {
+            color: #ffffff; 
+            background-color: #1F2937; 
+        }
+        .gray_box {
+            color: #1F2937; 
+            background-color: #cecece;
+        }
+    </style>
+    <title>Generátor harmonogramu</title>
+</head>
+<body>
+    <?php foreach ($days as $day => $events) { ?>
+        <h1 class="text-center font-['skautbold'] text-3xl mb-5"><?= $week_days[(new DateTime($day))->format('w')] . ' ' . (new DateTime($day))->format('d. m.') ?></h1>
+        <div id="boxes" class="flex gap-1 flex-col font-['themix']">
+        <!-- first two columns -->
+        <div class="grid grid-cols-[70%_30%]">
+                <div class="flex flex-col gap-1">
+                    <?php for ($i=0; $i < 2; $i++) { 
+                        printEvent(($events[$i][0]['PROGRAM'] ?? false), ($events[$i][0]['REQUIRED'] ?? false), $events[$i][0]['DTSTART'], $events[$i][0]['DTEND'], $events[$i][0]['SUMMARY'], $events[$i][0]['DESCRIPTION'] ?? null);
+                    } ?>
+            </div>
+                <div class="flex justify-center items-center">
+                    <div class="flex flex-row gap-1 justify-center items-center w-[85%]">
+                        <img class="h-12 aspect-square" src="icons/hand.png">
+                        <p class="text-[0.5rem]">Takto označený program souvisí s povinným úkolem ve stezce. K jeho splnění nebude další příležitost.</p>
+                    </div>
+                </div>
+            </div>
+        <?php for ($i=2; $i < count($events); $i++) { 
+            if (count($events[$i]) > 1) {
+                /* number of events in one group */
+                /* two events at the same time */
+                echo '<div class="grid grid-cols-2 gap-1">';
+                    //check if any of the arrays in the $events[$i] array is an array of arrays
+                    $isArrayOfArrays = false;
+                    foreach ($events[$i] as $event) {
+                        if (array_filter($event, 'is_array') === $event) {
+                            $isArrayOfArrays = true;
+                            break;
+                        }            
+                    }
+                    foreach ($events[$i] as $event) {
+                        if (array_filter($event, 'is_array') === $event) {
+                            echo '<div class="flex flex-col gap-1">';
+                            foreach ($event as $event) {
+                                printEvent(($event['PROGRAM'] ?? false), ($event['REQUIRED'] ?? false), $event['DTSTART'], $event['DTEND'], $event['SUMMARY'], $event['DESCRIPTION'] ?? null);
+                            }
+                            echo '</div>';
+                        } else {
+                            printEvent(($event['PROGRAM'] ?? false), ($event['REQUIRED'] ?? false), $event['DTSTART'], $event['DTEND'], $event['SUMMARY'], $event['DESCRIPTION'] ?? null, !$isArrayOfArrays ? 'h-fit' : '');
+                        }
+                    }
+                echo '</div>';
+                continue;
+            }
+            printEvent(($events[$i][0]['PROGRAM'] ?? false), ($events[$i][0]['REQUIRED'] ?? false), $events[$i][0]['DTSTART'], $events[$i][0]['DTEND'], $events[$i][0]['SUMMARY'], $events[$i][0]['DESCRIPTION'] ?? null);    
+        }
+        //if it's not last iteration, add page break
+        if($day != array_key_last($days)) {
+            echo '<div class="pagebreak"></div>';
+        }
+    }
+    ?>    
+</body>
+<script>
+    window.onload = function() { window.print(); }
+</script>
+</html>
